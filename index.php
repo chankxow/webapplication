a4b684ee237c695db33cb4f67ee084becf683676
KIATTIPOOM <101338054+chankxow@users.noreply.github.com>
2023-12-17 21:50:02 +0700

    x

--- index.php
+++ index.php
@@ -3,7 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>index</title>
+    <link rel="stylesheet" href="css/bootstrap.css" type="text/css">
+    <script src="js/bootstrap.bundle.js"></script>
 </head>
 <body>
     
