0300b7524ceca75a2de5c44fd78bbb9df13187c6
KIATTIPOOM <101338054+chankxow@users.noreply.github.com>
2023-12-17 22:19:21 +0700

    x

---
+++ admin-page.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <title>adminpage</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-light bg-light justify-content-between">
+        <div> 
+            <a class="navbar-brand">Navbar</a>
+            
+        </div>
+
+        <form action="POST" class="d-flex form-inline justify-content-between " style="width:200px;">
+            <a href="#" class="btn btn-outline-danger">ออกจากระบบ</a>
+            <a href="#" class="btn btn-outline-dark">Admin</a>
+        </form>
+    </nav>
+
+    <div class="container d-flex justify-content-center align-content-center ">
+
+    <div class="content text-center">
+        <h3>Hi welcome <span>Admin</span></h3>
+        <h1>Welcome</h1>
+        <p>This is User page</p>
+    </div>
+
+    </div>
+</body>
+
+</html>
