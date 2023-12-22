744f8c21a8900ef452862c579bc106c536ddf9b0
KIATTIPOOM <101338054+chankxow@users.noreply.github.com>
2023-12-22 19:45:55 +0700

    Update index.php

--- loginSystem/index.php
+++ loginSystem/index.php
@@ -3,9 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
 
-    <title>Document</title>
+<!-- HEADER -->
+
+    <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <script src="../node_modules/@popperjs/core/dist/umd/popper.min.js"></script>
+    <script src="../node_modules/@popperjs/core/dist/umd/popper.min.js"></script>
+    <script src="../node_modules/jquery/dist/jquery.min.js"></script>
+
+<!-- HEADER -->
+
+    <title>loginSystem</title>
 </head>
 <body>
     
