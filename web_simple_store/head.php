c2a4f038a2c89b3c55d1432c1da5397e02f793fb
KIATTIPOOM <101338054+chankxow@users.noreply.github.com>
2023-12-22 23:51:06 +0700

    x

---
+++ web_simple_store/head.php
@@ -0,0 +1,8 @@
+ <!-- HEADER -->
+
+ <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <script src="../node_modules/@popperjs/core/dist/umd/popper.min.js"></script>
+    <script src="../node_modules/@popperjs/core/dist/umd/popper.min.js"></script>
+    <script src="../node_modules/jquery/dist/jquery.min.js"></script>
+
+    <!-- HEADER -->
--- web_simple_store/index.php
+++ web_simple_store/index.php
@@ -5,15 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <!-- HEADER -->
-
-    <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.min.css">
-    <script src="../node_modules/@popperjs/core/dist/umd/popper.min.js"></script>
-    <script src="../node_modules/@popperjs/core/dist/umd/popper.min.js"></script>
-    <script src="../node_modules/jquery/dist/jquery.min.js"></script>
-
-    <!-- HEADER -->
-
+    <!--  -->
+    <?php require('head.php'); ?>
+    <!--  -->
     <title>loginSystem</title>
 </head>
 
@@ -22,8 +16,8 @@
     <div class="container">
 
         <form action="">
-            
-    
+
+
 
 
         </form>
